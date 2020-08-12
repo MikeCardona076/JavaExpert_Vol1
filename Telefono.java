dd9434c0e2269e0f783fff804d563ae39964cd71
mikecardona076 <mikecardona076@gmail.com>
2020-08-11 19:29:24 -0700

    1.1

--- Telefono.java
+++ Telefono.java
@@ -7,14 +7,34 @@ public class Telefono extends AgendaTelefonica{
     static HashMap<String, String> agenda = new HashMap<String, String>();
     static Scanner nuevotelefono = new Scanner(System.in);
     static AgendaTelefonica nuevousuario;
-    static String nombre, telefono;
+    static int eleccion;
+    static String nombre, telefono, buscar_nombre;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		do {
+			
+			System.out.println("Escoge una opción: " +
+					"\n 1. Agregar contacto " +
+				    "\n 2. Buscar contacto");
+					
+					eleccion = nuevotelefono.nextInt();
+					nuevotelefono.nextLine();
+					
+					if(eleccion == 1) {
+						AgregarTelefono();
+					}
+					if(eleccion == 2) {
+						BuscarTelefono();
+					}
+					
+		}
+		while(eleccion != 3);
+
 
 	}
 	
-	public void AgregarTelefono(){
+	public static  void AgregarTelefono(){
 		int llave, i;
 		do {
             System.out.println("Cuantos numeros vamos a registrar: ");
@@ -31,13 +51,18 @@ public class Telefono extends AgendaTelefonica{
     
             System.out.println("\n Ingresar Telefono");
             telefono = nuevotelefono.nextLine();
-     
-
             nuevousuario = new AgendaTelefonica();
             nuevousuario.SetNombre(nombre);
             nuevousuario.SetTelefono(telefono);
-   
-            agenda.put("England", "London");
+            agenda.put(nuevousuario.GetNombre(),nuevousuario.GetTelefono());
+	
+	    }
+	}
+	
+	public static void BuscarTelefono(){
+        System.out.println("Buscar contacto: ");
+        buscar_nombre = nuevotelefono.nextLine();
+        System.out.println(agenda.get(buscar_nombre));
 		
 	}
 
